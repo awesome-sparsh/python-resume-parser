5891ea12a03f4807c27ed29c72485f9219003857
awesome-sparsh <sparshsethi@outlook.com>
2024-10-11 17:16:59 +0530

    updated api key

--- components/llm_model.py
+++ components/llm_model.py
@@ -1,5 +1,5 @@
 from langchain_groq import ChatGroq
 
-llm = ChatGroq(temperature=0, model_name="llama3-8b-8192",groq_api_key="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",
+llm = ChatGroq(temperature=0, model_name="llama3-8b-8192",groq_api_key="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",
  model_kwargs={"response_format": {"type": "json_object"}})
 
